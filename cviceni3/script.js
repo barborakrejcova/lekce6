2a33de45bee1e21dc3a19e54e55be597d00dfae9
Barbora Krejčová <krejcova.barbora96@gmail.com>
2024-11-29 01:28:47 +0100

    cviceni3

---
+++ cviceni3/script.js
@@ -0,0 +1,20 @@
+//Obsah elipsy
+
+/*
+Z matematiky víme, že známe-li šířku a výšku elipsy, její obsah je polovina šířky krát polovina výšky krát číslo π (tj. přibližně 3,14).
+
+Založte si prázdnou stránku s JavaScriptovým souborem a napište funkci ellipseArea, která spočítá (vrátí) plochu pozemku dle zadané šířky a výšky. Číslo π najdete v JavaScriptu v proměnné Math.PI.
+
+Funkci ellipseArea otestujte (např. pomocí document.body.innerHTML += vypište výsledek volání pro různé hodnoty do stránky).
+
+Pro šířku 1 a výšku 2 by mělo vyjít 1.5707963267948966.
+Pro 2 a 2 pak 3.141592653589793.
+*/
+
+
+const ellipseArea = (width, height) => {
+    return (width / 2) * (height / 2) * Math.PI;
+};
+
+//console.log(ellipseArea(10, 20));
+document.body.innerHTML = `<p>${ellipseArea(10, 20)}</p>`;
